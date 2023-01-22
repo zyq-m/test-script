13106c3e57cea127ce86ee14a5f5c2c8bd044460
Haziq Musa <last.game01987@gmail.com>
2023-01-22 13:09:50 +0800

    move some value to env

--- src/main/java/com/softwaretesting/App.java
+++ src/main/java/com/softwaretesting/App.java
@@ -6,10 +6,10 @@ public final class App {
     public static void main(String[] args) {
         AddRecord m2 = new AddRecord();
 
-        m2.launch("https://www.oneplustwo.my/account/login");
+        m2.launch(System.getenv("LINK_1"));
         m2.start();
         m2.fillForm("Test Case 1", "Haziq", "Musa", "01234567890", "UniSZA", "tembila", "22200", true);
-        m2.fillForm("Test Case 2", true);
+        m2.fillForm("Test Case 2", "", "", "", "", "", "", true);
         m2.fillForm("Test Case 3", "Haziq", "Musa", "0123456", "UniSZA", "tembila", "22200", false);
         m2.fillForm("Test Case 4", "Haziq", "Musa", "0123456", "UniSZA", "tembila",
                 "zip12345678", false);
